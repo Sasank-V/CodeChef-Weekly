2cf743fd788875c4601fb0ad54b1b2520d52ce03
Sasank-V <sasanklearner@gmail.com>
2024-09-05 20:57:30 +0530

    Fixed Overflow for small screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
+import { useEffect, useState } from "react";
 import Leaderboard from "./Components/Leaderboard.jsx";
 import Navbar from "./Components/Navbar.jsx";
 import Topscore from "./Components/Topscore.jsx";
 
 function App() {
   return (<>
-  <div className="absolute w-[100%] h-[100%] bg-[#E7F0FE]">
-    <div className="w-[100%] relative h-[20%]">
+  <div className="absolute w-[100%] h-[100%] bg-[#E7F0FE] overflow-visible md:overflow-hidden">
+    <div className="w-[100%] relative h-[160px]">
       <Navbar/>
     </div>
-    <div className="flex h-[80%] flex-wrap overflow-hidden">
+    <div className="flex h-[80%] flex-wrap">
       <div className="w-[100%] md:w-[50%]">
         <Topscore/>
       </div>
