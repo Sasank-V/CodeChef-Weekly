cb59a422d2d81ad62d7936dee25f8c765f658b68
Sasank-V <sasanklearner@gmail.com>
2024-09-05 11:05:05 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Leaderboard from "./Components/Leaderboard.jsx";
+import Navbar from "./Components/Navbar.jsx";
+import Topscore from "./Components/Topscore.jsx";
+
+function App() {
+  return (<>
+  <div className="absolute w-[100%] h-[100%] bg-[#E7F0FE]">
+    <div className="w-[100%] relative h-[20%]">
+      <Navbar/>
+    </div>
+    <div className="flex h-[80%] flex-wrap">
+      <div className="w-[100%] md:w-[50%]">
+        <Topscore/>
+      </div>
+      <div className="w-[100%] md:w-[50%]">
+        <Leaderboard/>
+      </div>
+    </div>
+  </div>
+  </>);
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import backArrow from "../assets/Navbar/Back-Arrow.png";
+import fourDots from "../assets/Navbar/Four-Dots.png";
+
+const Navbar = () => {
+    const [slide, setSlide] = useState(16);
+
+    return (
+        <div className="flex flex-col justify-around items-center h-[100%] text-[#2C2C2C] font-inter">
+            <div className="flex justify-between items-center w-[100%] px-8 pt-2">
+                <div className="w-[12px]">
+                    <img src={backArrow} alt="Back Arrow" />
+                </div>
+                <div className="flex-col items-center border-2 h-[100%]">
+                    <h1 className="relative text-xl sm:text-[1.5rem] font-semibold bottom-[-2px]">
+                        Leaderboard
+                    </h1>
+                </div>
+                <div className="w-[20px]">
+                    <img src={fourDots} alt="Four Dots" />
+                </div>
+            </div>
+            <div className="bg-white px-2 py-4 relative top-[-5px] flex w-[80%] sm:w-[400px] rounded-2xl justify-around mt-2 text-sm lg:text-md font-semibold">
+                <div
+                    className="text-center cursor-pointer relative z-10"
+                    onClick={() => setSlide(16)}
+                >
+                    Organisation
+                </div>
+                <div
+                    className="text-center cursor-pointer relative z-10"
+                    onClick={() => setSlide(63.5)}
+                >
+                    Department
+                </div>
+                <div
+                    className="absolute bottom-[5px] h-[5%] bg-[#3B5987] transition-all duration-[400ms] ease-in-out w-[22%] z-0"
+                    style={{ left: `${slide}%` }}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
